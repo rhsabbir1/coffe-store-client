689cdb3a39fc5700d8843abee2506ed4fda71baa
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-09 12:12:01 +0600

    coffe added mongoBd by server

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h2>Update a coffe</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffee;    
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import AddCoffe from './components/AddCoffe.jsx'
+import UpdateCoffee from './components/UpdateCoffee.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App></App>
+  },
+  {
+    path:'addcoffe',
+    element:<AddCoffe></AddCoffe>
+  },
+  {
+    path:'updatecoffe',
+    element:<UpdateCoffee></UpdateCoffee>
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
